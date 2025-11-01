63d58451b85f8a0fe71b568b8a819a97ab1967f5
= <=>
2025-11-01 19:36:06 +0530

    added project skeleton, all backend services are running at 8080,8081,8082,8083

---
+++ backend/caretaker-service/src/main/java/com/colleger/caretaker/caretaker_service/CaretakerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.colleger.caretaker.caretaker_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaretakerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaretakerServiceApplication.class, args);
+	}
+
+}
---
+++ backend/teacher-service/src/test/java/com/colleger/teacher/teacher_service/TeacherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colleger.teacher.teacher_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
