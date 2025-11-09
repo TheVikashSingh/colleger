e9bdf4f114420a7ec12b84313886eca953b13738
= <=>
2025-11-09 22:45:32 +0530

    removed cors annotation

--- backend/student-service/src/main/java/com/colleger/student/student_service/controller/StudentsController.java
+++ backend/student-service/src/main/java/com/colleger/student/student_service/controller/StudentsController.java
@@ -11,7 +11,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/students/")
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class StudentsController {
 
     private final StudentsServiceImpl studentsService;
