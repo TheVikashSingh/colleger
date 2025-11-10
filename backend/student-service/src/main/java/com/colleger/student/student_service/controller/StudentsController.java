e53b871b0f55ff8454eb3932b197da46e9a47486
= <=>
2025-11-10 10:16:08 +0530

    changed controller route to solve the 404 issue

--- backend/student-service/src/main/java/com/colleger/student/student_service/controller/StudentsController.java
+++ backend/student-service/src/main/java/com/colleger/student/student_service/controller/StudentsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/students/")
+@RequestMapping("/api/students")
 @RequiredArgsConstructor
 public class StudentsController {
 
